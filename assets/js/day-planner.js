6e58880b9858b0ae6a2139970b0eee6e9e762db2
travis-john <travis.wl.john@gmail.com>
2019-12-08 19:08:47 -0500

    looping through available hours and scheduling elements

--- assets/js/day-planner.js
+++ assets/js/day-planner.js
@@ -1,3 +1,6 @@
+//global variables
+var scheduledHours = [];
+
 // adding clock to currentDay id
   function clock() {
     var dateString = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -5,3 +8,25 @@
   }
 
   setInterval(clock, 1000);
+
+//generating textareas for scheduling
+  for (var hour = 9; hour < 18; hour++) {
+      scheduledHours.push(moment({
+          hour
+      }).format('h  a'));
+      $('.container').append(`<div class="row time-block" data-time="${hour}">
+         <!--hour column-->
+             <div class="col-sm col-md-2 hour">
+               <p class=dayHour>${moment({hour}).format('h  a')}</p>
+             </div>
+
+         <!--user input text area-->
+             <div class="col-sm col-md-8 d-flex description">
+               <textarea class=textArea></textarea>
+             </div>
+
+         <!--save button-->
+             <div class="col-sm col-md-2 saveBtn">
+             <i class="far fa-save fa-2x" id=icon></i>
+             </div>`);
+  }
