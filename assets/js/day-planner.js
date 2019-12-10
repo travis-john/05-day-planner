ec7c40de51a71c08b1aba0a8093cbf98a0faa6e4
travis-john <travis.wl.john@gmail.com>
2019-12-10 10:03:49 -0500

    Update day-planner.js

--- assets/js/day-planner.js
+++ assets/js/day-planner.js
@@ -49,8 +49,21 @@ $.each($('.time-block'), function(index, value) {
       $(this).find('textarea').addClass('future');
     }
 });
+function checkTime() {
+  $.each($('.time-block'), function(index, value) {
+  let dateHour = $(value).attr('data-time');
+   if (Number(dateHour) === m.hour()) {
+      $(this).find('textarea').addClass('present');
+    } else if (Number(dateHour) < m.hour()) {
+      $(this).find('textarea').addClass('past').attr('disabled', 'disabled');
+      $(this).find('.save-button').addClass('disabled').attr('disabled', true);
+    } else {
+      $(this).find('textarea').addClass('future');
+    }
+  });
+}
 
-// setInterval(checkTime, 60*60*1000);
+setInterval(checkTime, 60*60*1000);
 
 console.log(workDayStart);
 console.log(currentTime);
